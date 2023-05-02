2b3628d95878d7f6419ba396b85e7a48c8d4cab2
Rubaiyatrafi <rubaiyatkabirrafi9166@gmail.com>
2023-05-02 19:23:05 +0600

    home page create

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,10 +1,33 @@
+/* eslint-disable react/no-unescaped-entities */
 import React from "react";
 import "./Home.css";
+import image from "../../../assets/food-1.jpg";
+import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
     <div>
-      <h1>This is Home</h1>
+      <div className="title bg-slate-100 justify-around mt-10 ml-5 mr-5 rounded-lg p-10">
+        <div className="flex justify-center items-center">
+          <h1 className="text-5xl font-serif font-semibold text-rose-900">
+            It's not Just <br /> Food, It's an <br />
+            <span className="text-6xl font-sans font-bold text-rose-600">
+              Experience
+            </span>
+            <br />
+            <span className="text-lg text-slate-700">
+              We are the most fastest and favorite Chef <br /> delivery service
+              all over the world. <br /> Search for your favourite Chef. <br />
+            </span>
+            <Link className="btn btn-secondary" to="/about">
+              About
+            </Link>
+          </h1>
+        </div>
+        <div>
+          <img className="rounded-lg" src={image} alt="" />
+        </div>
+      </div>
     </div>
   );
 };
--- src/pages/Shared/Header/Header.jsx
+++ src/pages/Shared/Header/Header.jsx
@@ -6,14 +6,14 @@ import logo from "../../../assets/5493254.jpg";
 const Header = () => {
   return (
     <div>
-      <nav className="navbar m-5 flex">
+      <nav className="navbar  mt-10 md:flex-row space-x-40 justify-center flex-col">
         <div>
-          <img className="w-12 rounded-lg m-3" src={logo} alt="" />
-          <h2 className="m-2 text-5xl font-bold font-serif text-blue-950">
+          <img className="w-12 rounded-lg" src={logo} alt="" />
+          <h2 className="ml-2 mr-10 text-5xl font-bold font-serif text-blue-950 title-name">
             Best <span className="text-rose-700">Chef</span> Today
           </h2>
         </div>
-        <div className="text-2xl ml-40 font-bold font-serif">
+        <div className="text-2xl font-bold font-serif  md:flex-row flex-col link-nav">
           <NavLink to="/">Home</NavLink>
           <NavLink to="/about">About</NavLink>
           <NavLink to="/blog">Blog</NavLink>
