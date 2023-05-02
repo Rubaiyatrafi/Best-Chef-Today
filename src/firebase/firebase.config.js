6137d12a09e8069d92f0df60fef349f63eea84f2
Rubaiyatrafi <rubaiyatkabirrafi9166@gmail.com>
2023-05-02 09:05:35 +0600

    first commit

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCYbzZqARFo_d6yAVXqkL6EdkTHVil9YQc",
+  authDomain: "best-chef-today.firebaseapp.com",
+  projectId: "best-chef-today",
+  storageBucket: "best-chef-today.appspot.com",
+  messagingSenderId: "550176221932",
+  appId: "1:550176221932:web:2abd80325de0072b4613c9",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
